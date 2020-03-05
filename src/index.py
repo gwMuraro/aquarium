a9402d08cf4cd725ca13745850f5d5eab3fcdae9
gwMuraro <gw.muraro@gmail.com>
2020-03-05 23:51:15 +0000

    Mise en place de l'horloge

--- src/index.py
+++ src/index.py
@@ -1,33 +1,49 @@
 import pygame
 
+
 pygame.init()
 
-# === Fenêtre 
+# --- Fenêtre 
 largeur, hauteur = 800, 600
 fenetre = pygame.display.set_mode((largeur, hauteur))
 pygame.display.set_caption("Aquarium")
-# Fenêtre ===
+# Fenêtre ---
 
-# === Image
+# --- Horloge
+# L'utilisation des images par secondes nous permettent de déterminer 
+# les secondes dans la mainloop
+horloge = pygame.time.Clock()
+FPS = 24 
+# Horloge ---
+
+# --- Image
 image_poisson = pygame.image.load("src/images/fish.png")
 image_poisson = pygame.transform.scale(image_poisson, (60, 40))
-# Image ===
+# Image ---
 
 # ========== MAIN LOOP =========
 bContinue = True
 while bContinue : 
     pygame.time.delay(100)
 
-    # === Evenements 
+    # --- Evenements 
     for event in pygame.event.get() :
         if event.type == pygame.QUIT : 
             bContinue = False
-    # Evenements ===
+    # Evenements ---
 
-    # === Dessin 
+    # --- Dessin 
     fenetre.blit(image_poisson, (200, 200)) # dessine le poisson
     pygame.display.flip()
-    # Dessin ===
+    # Dessin ---
+
+    # --- Horloge
+    horloge.tick(FPS)
+    # Horloge ---
+
+
+
+
 print("Nous quittons notre aquarium")
 
 pygame.quit()
