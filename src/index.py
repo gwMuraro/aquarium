678ca0c2b0aea9ef47fff72b7e8a752412583aea
gwMuraro <gw.muraro@gmail.com>
2020-03-05 21:58:32 +0000

    Squelette et premiers developpements pour le test de l'architecture des fichiers.

--- src/index.py
+++ src/index.py
@@ -2,5 +2,32 @@ import pygame
 
 pygame.init()
 
-while True :
-    print("Boeuf")
+# === Fenêtre 
+largeur, hauteur = 800, 600
+fenetre = pygame.display.set_mode((largeur, hauteur))
+pygame.display.set_caption("Aquarium")
+# Fenêtre ===
+
+# === Image
+image_poisson = pygame.image.load("src/images/fish.png")
+image_poisson = pygame.transform.scale(image_poisson, (60, 40))
+# Image ===
+
+# ========== MAIN LOOP =========
+bContinue = True
+while bContinue : 
+    pygame.time.delay(100)
+
+    # === Evenements 
+    for event in pygame.event.get() :
+        if event.type == pygame.QUIT : 
+            bContinue = False
+    # Evenements ===
+
+    # === Dessin 
+    fenetre.blit(image_poisson, (200, 200)) # dessine le poisson
+    pygame.display.flip()
+    # Dessin ===
+print("Nous quittons notre aquarium")
+
+pygame.quit()
