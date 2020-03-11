e6d62e7d04b3b9652de54e72c701c6aa667fed28
gwMuraro <gw.muraro@gmail.com>
2020-03-11 14:31:31 +0100

    refactor 1 1 ajout des fichiers init

---
+++ src/utils/ControlleurEntites/__init__.py
@@ -0,0 +1,5 @@
+from utils.ControlleurEntites.ContenantVivant import * 
+from utils.ControlleurEntites.Decorateur import *
+from utils.ControlleurEntites.DecorationPredateur import *
+from utils.ControlleurEntites.DecorationProie import *
+from utils.ControlleurEntites.Poisson import *
---
+++ src/utils/ObjetsSprite/__init__.py
@@ -0,0 +1,5 @@
+from utils.ObjetsSprite.ObjetsSprite import *
+from utils.ObjetsSprite.SpriteBase import *
+from utils.ObjetsSprite.SpriteBoutton import *
+from utils.ObjetsSprite.SpritePiranha import *
+from utils.ObjetsSprite.SpritePoisson import *
