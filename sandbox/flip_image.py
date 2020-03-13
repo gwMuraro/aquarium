fd6611b763c53c58cea3c118ad33445b33c88e44
Kagari <aymeric.adenot27@orange.fr>
2020-03-13 17:41:28 +0000

    changement de l'image des crevettes

--- sandbox/flip_image.py
+++ sandbox/flip_image.py
@@ -4,8 +4,8 @@ from PIL import Image
 #  base qu'on va utiliser pour le début du développement 
 ### 
 
-img = Image.open("../images/meduse.png")
-img.save("../images/meduse_vers_la_droite.png")
+img = Image.open("../images/crevette.png")
+img.save("../images/crevette_vers_la_droite.png")
 
 img2 = img.transpose(Image.FLIP_LEFT_RIGHT)
-img2.save("../images/meduse_vers_la_gauche.png")
+img2.save("../images/crevette_vers_la_gauche.png")
